ac3aae75c4ce94eb78f3a802c802388053f68765
marianalarag <marianalarag@outlook.com>
2025-02-12 09:00:31 -0600

    Texto principal añadido

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,45 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        //Ícono de la izquierda
+        leading: Icon(
+          Icons.menu,
+          color: Colors.grey[800],
+        ),
+        actions: [Padding(
+          padding: const EdgeInsets.only(right: 24.0),
+          child: Icon(Icons.person),
+        )],
+      ),
+      body: Column(
+        children: [
+          //Texto "I want to eat"
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 36, vertical : 18),
+            child:Row(
+              children: [
+                Text("I want to ",style: TextStyle(fontSize: 32),),
+                Text("Eat", style: TextStyle(
+                  //Tamaño de letra
+                  fontSize: 32,
+                  //Negritas
+                  fontWeight: FontWeight.bold,
+                  //Subrayado
+                  decoration: TextDecoration.underline
+                ),)
+              ],
+            ))
+            //Tab bar
+
+
+            //Tab bar view
+
+            //Carrito
+          ],
+        ),
+      );
+    }
   }
-}
